4de436a916d7a4a886a927f65748ed41a8d97ea4
Allan Jardine <allan.jardine@sprymedia.co.uk>
2015-09-11 10:18:52 +0100

    Easier includes of the library
    Previously you would need to include each of the files from this repo
    individually in the script(s) that actually use the files. While easy
    enough to do, it would be easier to simply include a single base file
    and have it perform the required includes for us.
    
    I've added this as a separate file rather than into Client.php to
    preserve backwards compatibility. New users can include KM.php while the
    folks including the files individually can ignore the new file and carry
    on with the old way.

---
+++ src/KISSmetrics/KM.php
@@ -0,0 +1,10 @@
+<?php
+
+// Include dependencies classes for easy include
+require_once( dirname(__FILE__).'/Client.php' );
+require_once( dirname(__FILE__).'/ClientException.php' );
+require_once( dirname(__FILE__).'/Transport/Transport.php' );
+require_once( dirname(__FILE__).'/Transport/TransportException.php' );
+require_once( dirname(__FILE__).'/Transport/Sockets.php' );
+require_once( dirname(__FILE__).'/Transport/Delayed.php' );
+require_once( dirname(__FILE__).'/Transport/Mock.php' );
